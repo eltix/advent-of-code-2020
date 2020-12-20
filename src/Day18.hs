ef6a0083148cc28be92f1acf992789888ed9acb0
Eliott Tixier <eliott.tixier@gmail.com>
2020-12-20 17:12:23 +0100

    feat(day18): solutions to part 1 and 2

--- src/Day18.hs
+++ src/Day18.hs
@@ -1,17 +1,17 @@
 module Day18 where
 
 import           BasicPrelude
-
-import LoadAndParse
+import           Control.Monad.Combinators.Expr
+import           LoadAndParse
 
 computeSolutions :: IO (Int, Int)
 computeSolutions = do
-  -- equations <- loadAndParseAsRows parseEquation "inputs/day18.txt"
-  let
-    -- part 1
-    sol1 = 0 -- sum . fmap evaluate $ equations
-    -- part 2
-    sol2 = 0
+  -- part 1
+  equations <- loadAndParseAsRows (parseEquation rulesPart1) "inputs/day18.txt"
+  let sol1 = sum . fmap evaluate $ equations
+  -- part 2
+  equations' <- loadAndParseAsRows (parseEquation rulesPart2) "inputs/day18.txt"
+  let sol2 = sum . fmap evaluate $ equations'
   return (sol1, sol2)
 
 data Equation = Leaf Int | Op Op Equation Equation
@@ -19,22 +19,31 @@ data Equation = Leaf Int | Op Op Equation Equation
 data Op = Plus | Times deriving Show
 
 evaluate :: Equation -> Int
-evaluate (Leaf x) = x
-evaluate (Op Plus x y) = evaluate x + evaluate y
+evaluate (Leaf x)       = x
+evaluate (Op Plus x y)  = evaluate x + evaluate y
 evaluate (Op Times x y) = evaluate x * evaluate y
 
-eq :: Text
-eq = "(5 * 6 * 5 * 7) + (6 + (8 * 3 * 9 + 2 + 7) + 7 + (4 * 2 + 5)) + 8"
+-- | Believe it or not I had the hardest time implementing an equation parser,
+-- especially because of these forsaken parentheses.
+-- I gave up and used 'Control.Monad.Combinators.Expr.makeExprParser' instead
+-- which turned out to be extremely easy and satisfying.
+parseEquation :: [[Operator Parser Equation]] -> Parser Equation
+parseEquation rules = makeExprParser term rules
+  where
+    term :: Parser Equation
+    term = parens (parseEquation rules) <|> (Leaf <$> int) <?> "term"
 
-parseEquation :: Parser Equation
-parseEquation = do
-  a  <- Leaf <$> int
-  op <- parseOp
-  b  <- try parseEquation <|> Leaf <$> int
-  pure $ Op op b a
+-- | + and * are left-associative and have same precedence level
+rulesPart1 :: [[Operator Parser Equation]]
+rulesPart1 =
+  [ [ InfixL ((Op Plus) <$ symbol "+"), InfixL ((Op Times) <$ symbol "*") ] ]
+
+-- | same as part1 but + has precedence over *
+rulesPart2 :: [[Operator Parser Equation]]
+rulesPart2 =
+  [ [InfixL ((Op Plus) <$ symbol "+") ]
+  , [InfixL ((Op Times) <$ symbol "*")]
+  ]
 
 parens :: Parser a -> Parser a
-parens = between (symbol ")") (symbol "(")
-
-parseOp :: Parser Op
-parseOp = lexeme $ choice [Plus <$ char '+', Times <$ char '*']
+parens = between (symbol "(") (symbol ")")
